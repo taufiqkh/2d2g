e21e7489d2d5e0cfc9b65de17b65618dbfe6bc71
Taufiq Hoven <taufiq.hoven@gmail.com>
2014-07-16 20:45:14 +1000

    Fixed lat/long to pixel translation, added prototype graticule

--- FlatPixelProjection.js
+++ FlatPixelProjection.js
@@ -1,13 +1,13 @@
 /**
- * EuclideanProjection
+ * FlatPixelProjection
  *
  * Apply it with this:
- * myCustomMapType.prototype.projection = new EuclideanProjection();
+ * myCustomMapType.prototype.projection = new FlatPixelProjection();
  *
  * Modified from https://groups.google.com/forum/#!topic/google-maps-js-api-v3/UEIbpcTdMHs
  * Adapted to remove latitude distortion.
  */
-function EuclideanProjection(scaleLat, scaleLng, offsetLat, offsetLng) {
+function FlatPixelProjection(scaleLat, scaleLng, offsetLat, offsetLng) {
         var EUCLIDEAN_RANGE = 256;
         this.pixelOrigin_ = new google.maps.Point(EUCLIDEAN_RANGE / 2, EUCLIDEAN_RANGE / 2);
         this.pixelsPerLonDegree_ = EUCLIDEAN_RANGE / 360;
@@ -18,7 +18,7 @@ function EuclideanProjection(scaleLat, scaleLng, offsetLat, offsetLng) {
         this.offsetLng = offsetLng;      // Width - direct offset +/-
 };
 
-EuclideanProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
+FlatPixelProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
         var point = opt_point || new google.maps.Point(0, 0);
 
         var origin = this.pixelOrigin_;
@@ -26,18 +26,18 @@ EuclideanProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
         // NOTE(appleton): Truncating to 0.9999 effectively limits latitude to
         // 89.189.  This is about a third of a tile past the edge of the world
         // tile.
-        var unscaledY = Math.sin(latLng.lat() * Math.PI / 180)
+        var unscaledY = Math.sin((latLng.lat() + this.offsetLat) * Math.PI / 180)
         if (unscaledY > 0.9999) {
             unscaledY = 0.9999;
         } else if (unscaledY < -0.9999) {
             unscaledY = -0.9999;
         }
-        var radiansY = Math.log((1 + unscaledY) / (1 - unscaledY));
-        point.y = radiansY * this.scaleLat * this.pixelsPerLonRadian_;
+        var radiansY = Math.log((1.0 + unscaledY) / (1.0 - unscaledY));
+        point.y = Math.ceil(radiansY * this.scaleLat * this.pixelsPerLonRadian_);
         return point;
 };
 
-EuclideanProjection.prototype.fromPointToLatLng = function(point) {
+FlatPixelProjection.prototype.fromPointToLatLng = function(point) {
         var me = this;
 
         var origin = me.pixelOrigin_;
